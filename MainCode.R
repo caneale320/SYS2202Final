39e25eb11a153bdd9b6ebea5d43e773f14ca9836
Stuart Keith <sjk4qk@virginia.edu>
2020-04-08 16:04:03 +0000

    Set up libraries

--- MainCode.R
+++ MainCode.R
@@ -10,3 +10,14 @@ Steven's test'
 >>>>>>> 44e59317fb73197472fc3fa8d34e7b4645379385
 
 Test - Stuart
+
+#Libraries needed for shiny
+library(shiny)
+library(shinythemes)
+library(dplyr)
+library(readr)
+
+#Defining the server
+server <- function( input, output ){
+  
+}
