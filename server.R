e0055c9b18d8f7451e179c2672de3843c708bb24
Stuart Keith <sjk4qk@virginia.edu>
2020-04-16 11:56:06 +0000

    Reactive server plot

--- server.R
+++ server.R
@@ -12,18 +12,18 @@ shinyServer(function(input,output) {
   selectedtrends <- reactive({
     req(input$sector)
     req(input$covid)
+    req(input$marketdata)
+    
+    frame <- input$marketdata
+    
+    variable <- input$sector
   })
   
-  output$FXResults <- renderPlot({
-    
-    #currency = Quandl(input$symbols,start_date=input$start_date , end_date=input$end_date,type = "xts")
-    #currencydf = data.frame(currency)
+  output$plot <- renderPlot({
     simplemovingaverage=SMA(Communications$XLC.Close,n=20)
     
    
-    plot(Communications$XLC.Close,type="l",col="blue",xlab="Days",ylab="Price")
-    lines(simplemovingaverage,type = "l",col="red")
-    title(main=input$symbols)
+    plot(x = frame$variable
     
       
   })
